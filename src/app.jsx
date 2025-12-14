8e850dd82369b46fd86472e84c032c699ac28560
alex baldwin <ghostbaldwin@gmail.com>
2025-12-14 15:18:52 -0500

    update assets

--- src/app.jsx
+++ src/app.jsx
@@ -36,11 +36,10 @@ export default function Home() {
         <br />
         <a
           className="btn--remix"
-          target="_top"
-          href="https://glitch.com/edit/#!/remix/glitch-hello-react"
+          target="_blank"
+          href="https://github.com/cubeghost/afffirmations"
         >
-          <img src="https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FLogo_Color.svg?v=1618199565140" alt="" />
-          Remix on Glitch
+          github
         </a>
       </footer>
     </StateProvider>
--- src/components/Meta.jsx
+++ src/components/Meta.jsx
@@ -1,40 +1,44 @@
 import * as React from "react";
-import SEO from "../seo.json";
 import { Helmet } from 'react-helmet-async';
+import thumbnail from "../assets/preview.png"
+
+const seo = {
+  "title": "afffirmations generator",
+  "description": "“global self hypnosis” daily affirmation meme generator. inspired by @afffirmations",
+  "image": thumbnail
+};
 
 const Meta = () => {
-  // If url is set to 'glitch-default', we use the hostname for the current page
-  // Otherwise we use the value set in seo.json
-  const url = SEO.url === 'glitch-default' ? window.location.hostname : SEO.url
+  const url = window.location.hostname
   
   // React Helmet manages the content of the page head such as meta tags
   // We use the async package https://github.com/staylor/react-helmet-async
   return <Helmet>
-    <title>{SEO.title}</title>
+    <title>{seo.title}</title>
 
     <meta
       name="description"
-      content={SEO.description}
+      content={seo.description}
     />
     <meta name="robots" content="index,follow" />
     <link rel="canonical" href={url} />
-    <meta property="og:title" content={SEO.title} />
+    <meta property="og:title" content={seo.title} />
     <meta property="og:type" content="article" />
     <meta property="og:url" content={url} />
     <meta
       property="og:description"
-      content={SEO.description}
+      content={seo.description}
     />
     <meta
       property="og:image"
-      content={SEO.image}
+      content={seo.image}
     />
 
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:creator" content="@cubeghost" />
-    <meta name="twitter:title" content={SEO.title} />
-    <meta name="twitter:description" content={SEO.description} />
-    <meta name="twitter:image" content={SEO.image} />
+    <meta name="twitter:title" content={seo.title} />
+    <meta name="twitter:description" content={seo.description} />
+    <meta name="twitter:image" content={seo.image} />
 
   </Helmet>
 };
